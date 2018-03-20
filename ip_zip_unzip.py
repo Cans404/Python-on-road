4bb33a95eaba74e277c9c857415c327499b8b795
Cans Fong <cans_fong@163.com>
2018-03-21 00:47:26 +0800

    add unzip section

--- ip_zip_unzip.py
+++ ip_zip_unzip.py
@@ -32,5 +32,34 @@ def ip_encrypt(ip) :
 	ip_code = ip_code_zip.replace("1", "")
 	print "4. %-45s ------> %45s" % (ip_code_zip, ip_code)
 
+def func(m) :
+	if m.expand(r"\g<dig>") == "" :
+		mark = ""
+	else:
+		mark = "*"
+	return m.expand(r"\g<dig>") + mark + r"'" + m.expand(r"\g<chr>") + r"'" + "+"
+
+def ip_decrypt(ip_code) :
+	cmd = re.sub(r"(?P<dig>\d*)(?P<chr>[ab])", func, ip_code).strip("+")
+	ip_code_str = eval(cmd)
+	print "1. %-45s ------> %45s" % (ip_code, ip_code_str)
+
+	ip_code_bin = re.sub(r"\w", lambda m: chr(ord(m.group()) - 49), ip_code_str)
+	print "2. %-45s ------> %45s" % (ip_code_str, ip_code_bin)
+
+	ip_decs = []
+
+	for i in range(0, 32, 8) :
+		ip_bin = ip_code_bin[i:i+8].lstrip("0")
+		ip_dec = int(ip_bin, 2)
+		ip_decs.append(str(ip_dec))
+
+	ip = ".".join(ip_decs)
+	print "3. %-45s ------> %45s" % (ip_code_bin, ip)
+
 if __name__ == '__main__' :
 	ip_encrypt("1.1.1.129")
+	print ""
+	print "<<" + "#" * 98 + ">>"
+	print ""
+	ip_decrypt("7ab7ab7a2b6ab")
